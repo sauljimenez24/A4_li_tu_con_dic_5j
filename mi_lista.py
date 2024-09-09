a7a35201b07b0f42b909d3f940dfa4daf82429b5
sauljimenez24 <a.22308051281222@cbtis128.edu.mx>
2024-09-09 10:13:49 -0600

    Add files via upload

---
+++ mi_lista.py
@@ -0,0 +1,18 @@
+print("ejemplo de lista")
+arcoiris=["verde", "azul", "morado"]
+
+print(arcoiris)
+longitud=len(arcoiris)
+print("Elementos que contiene la lista arcoiris ", longitud)
+print(f"Elementos que contiene la lista arcoiris v2 {longitud}")
+print("accediendo a un elemento de la lista ")
+print(arcoiris[1])
+print(f"elemento en la posicion 0 es {arcoiris[0]}")
+print("agregar un elemento de la lista ")
+print(arcoiris)
+arcoiris.append("naranja")
+print(arcoiris)
+print("Listar los elemtos de la lista ciclo for ")
+for jimenez in arcoiris:
+    print(jimenez)
+    
---
+++ mi_tuplas.py
@@ -0,0 +1,8 @@
+print("Ejemplo de tuplas")
+canciones=("Te amo" ,"El NoNoa", "Amor eterno")
+print(canciones)
+y = list(canciones)
+print(y)
+y[1] = "La puerta negra"
+x = tuple(y)
+print(x)
