80c08a52a329625ac34e9f46fda4d9f40d8c9735
sauljimenez24 <a.22308051281222@cbtis128.edu.mx>
2024-09-11 07:37:51 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,7 @@
+print("Ejemplo conjunto set")
+tipodeportes={"futbol", "tenis", "basquetbol"}
+print(tipodeportes)
+print("Lista elementos del conjunto tipodeportes loop for")
+print(tipodeportes)
+for jimenez in tipodeportes:
+    print(jimenez)
